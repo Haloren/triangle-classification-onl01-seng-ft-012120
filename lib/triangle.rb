fe4cba05ac2545a5b62691f1a14df099309c3bcb
Kyle McKenna <mckennakyler@gmail.com>
2020-02-09 04:41:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
      
     if @side_1 <= 0 || @side_2 <= 0 ||@side_3 <= 0 #each side must be >= 0
       raise TriangleError
-    elsif @side_1 + @side_2 > @side_3 || @side_2 <= 0 ||@side_3 <= 0 #sum of 2 side length > 1 side length 
+    elsif @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3<= @side_1 #sum of 2 side length <= 1 side length 
       raise TriangleError
     end 
      
