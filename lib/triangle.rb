d81c0b911a08457925277ca912cc6db215ecb3b0
Kyle McKenna <mckennakyler@gmail.com>
2020-02-09 04:27:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,10 @@ class Triangle
      
     if @side_1 == @side_2 && @side_2 == @side_3 #equilateral: all sides are the same length side != side  
       :equilateral
-    elsif @side_1 != @side_2 && @side_2 != @side_3 #scalene: no sides are the same length side == side
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3 #scalene: no sides are the same length side == side
       :scalene
-    #isosceles: 2 sides are the same length (only other option)
+    else #isosceles: 2 sides are the same length
+      :isosceles
     end 
     #each side must be >= 0, sum of 2 side length > 1 side length  
   end   
