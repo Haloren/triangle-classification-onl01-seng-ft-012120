72e45f81286220bb7de31e6ec75ba4533126f5d5
Kyle McKenna <mckennakyler@gmail.com>
2020-02-09 04:18:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     #equilateral: all sides are the same length side != side  
     #isosceles: 2 sides are the same length (only other option)
     
-    #each side must be > 0
+    #each side must be >= 0, sum of 2 side length > 1 side length  
   end   
   
   class TriangleError < StandardError
